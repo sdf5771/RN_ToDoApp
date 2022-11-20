746579cb4f7f024eac52be825d732094698fcb1d
seobisback <66990091+sdf5771@users.noreply.github.com>
2022-11-20 18:53:21 +0900

    20221120PM1853

--- App.js
+++ App.js
@@ -6,6 +6,10 @@ import {theme} from "./color";
 import { Fontisto } from '@expo/vector-icons';
 
 const STORAGE_KEY = "@toDos"
+const completeIconToDo = {
+  complete : "checkbox-active",
+  incomplete : "checkbox-passive"
+}
 
 export default function App() {
   const [working, setWorking] = useState(true);
@@ -21,7 +25,9 @@ export default function App() {
     const loadToDos = async() => {
       try{
         const stringObj = await AsyncStorage.getItem(STORAGE_KEY);
-        setToDos(JSON.parse(stringObj));
+        if(stringObj){
+          setToDos(JSON.parse(stringObj));
+        }
       }catch(e){
         console.log(e)
       }
@@ -33,7 +39,7 @@ export default function App() {
     if (text === "") return;
 
     //Save ToDo Logic
-    const newToDos = Object.assign({}, toDos, {[Date.now()] : {text, work: working}})
+    const newToDos = Object.assign({}, toDos, {[Date.now()] : {text, work: working, complete: "incomplete"}})
     setToDos(newToDos)
     await saveToDos(newToDos);
     //text empty setting
@@ -49,15 +55,31 @@ export default function App() {
           }}, {text: "Cancel"}])
 
   }
+  const completeToDo = async (key) => {
+    const newToDos = {...toDos}
+    newToDos[key].complete = newToDos[key].complete === 'incomplete' ? 'complete' : 'incomplete'
+    setToDos(newToDos)
+    await saveToDos(newToDos);
+  }
+  const modifyToDo = async (key) => {
+    Alert.prompt("Edit To Do", 'Edit To Do Text', [{text: 'Edit', onPress: async(editText) => {
+        const newToDos = {...toDos}
+        newToDos[key].text = editText
+        setToDos(newToDos)
+        await saveToDos(newToDos);
+      }}, {text: 'cancel'}])
+  }
   return (
     <View style={styles.mainContainer}>
       <StatusBar style="auto" />
       <View style={styles.mainHeader}>
-        <TouchableOpacity onPress={work}>
-          <Text style={{...styles.btnText, color: working ? "white" : theme.grey}}>Work</Text>
+        <TouchableOpacity style={{flexDirection: "row", alignItems: "center", justifyContent: "space-between"}} onPress={work}>
+          <Text style={{...styles.btnText, marginRight: 10, color: working ? "white" : theme.grey}}>Work</Text>
+          <Fontisto name="nervous" size={24} color={working ? "white" : theme.grey} />
         </TouchableOpacity>
-        <TouchableOpacity onPress={travel}>
-          <Text style={{...styles.btnText, color: !working ? "white" : theme.grey}}>Travel</Text>
+        <TouchableOpacity style={{flexDirection: "row", alignItems: "center", justifyContent: "space-between"}} onPress={travel}>
+          <Fontisto name="laughing" size={24} color={!working ? "white" : theme.grey} />
+          <Text style={{...styles.btnText, marginLeft: 10, color: !working ? "white" : theme.grey}}>Travel</Text>
         </TouchableOpacity>
       </View>
       <TextInput value={text} returnKeyType="done" onSubmitEditing={addToDo} onChangeText={TextInputOnChangeHandler} placeholder={working ? "Add a To Do" : "Where do you wanna go?"} style={styles.input} />
@@ -66,7 +88,12 @@ export default function App() {
           return (
               toDos[key].work === working ?
               <View style={styles.toDo} key={key}>
-                <Text style={styles.toDoText}>{toDos[key].text}</Text>
+                <TouchableOpacity onPress={() => completeToDo(key)}>
+                  <Fontisto name={toDos[key].complete ? completeIconToDo[toDos[key].complete] : completeIconToDo["incomplete"]} size={18} color="white" />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => modifyToDo(key)}>
+                  <Text style={toDos[key].complete === 'incomplete' ? styles.toDoText : styles.toDoTextComplete}>{toDos[key].text}</Text>
+                </TouchableOpacity>
                 <TouchableOpacity onPress={() => deleteToDo(key)}>
                   <Fontisto name="trash" size={18} color={theme.grey} />
                 </TouchableOpacity>
@@ -116,5 +143,11 @@ const styles = StyleSheet.create({
     color: "white",
     fontSize: 16,
     fontWeight: "500",
+  },
+  toDoTextComplete: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "500",
+    textDecorationLine: 'line-through',
   },
 });
